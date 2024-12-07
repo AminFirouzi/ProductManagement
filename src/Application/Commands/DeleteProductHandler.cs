664736d9cc181d6ffefc82d5f85cde71eadf9406
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-12-07 17:57:03 +0330

    DeleteProduct Command and handler implemented

---
+++ src/Application/Commands/DeleteProductHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands
+{
+    public class DeleteProductCommandHandler : IRequestHandler<DeleteProductCommand>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public DeleteProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetProductByEmailAndDateAsync(request.ManufactureEmail, request.ProduceDate);
+            if (product == null)
+            {
+                throw new KeyNotFoundException("Product not found.");
+            }
+
+            await _productRepository.DeleteProductAsync(product);
+            return Unit.Value;
+        }
+
+        Task IRequestHandler<DeleteProductCommand>.Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
