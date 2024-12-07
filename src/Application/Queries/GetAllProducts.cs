c15472f30e7dca8b848863f5ad651794057b9f45
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-12-07 18:11:12 +0330

    completed Get Queries for products

---
+++ src/Application/Queries/GetAllProducts.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetAllProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetAllProductsAsync();
+        }
+    }
+}
