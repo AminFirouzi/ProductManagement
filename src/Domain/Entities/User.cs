14cc759e58cb6163293db4ac3a2692e0a56421ed
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-12-07 23:02:07 +0330

    update user entity and added Configuration for product

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        // Primary Key: Email
+        public string Email { get; set; } = string.Empty;
+
+        // Navigation Property: One User can have many Products
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
---
+++ src/Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // Composite primary key
+            builder.HasKey(p => new { p.ManufactureEmail, p.ProduceDate });
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(p => p.ManufacturePhone)
+                .HasMaxLength(50);
+
+            builder.Property(p => p.ManufactureEmail)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.IsAvailable)
+                .IsRequired();
+
+            // Relationship with User
+            builder.HasOne(p => p.User)
+                .WithMany(u => u.Products)
+                .HasForeignKey(p => p.ManufactureEmail)
+                .HasPrincipalKey(u => u.Email)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
