43dbbc6e211ed67fc8747defddc648e46bf47eb9
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-12-08 00:04:54 +0330

    added product Repository

---
+++ src/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByEmailAndDateAsync(string email, DateTime produceDate)
+        {
+            return await _context.Products
+                .FirstOrDefaultAsync(p => p.ManufactureEmail == email && p.ProduceDate == produceDate);
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProductAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
