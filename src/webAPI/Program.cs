9933342ffca61402078e559692cf4fd65f39638b
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-12-08 01:30:17 +0330

    added initial migration

--- src/webAPI/Program.cs
+++ src/webAPI/Program.cs
@@ -1,15 +1,37 @@
+using Application.Commands;
+using Application.Interfaces;
+using Application.Validators;
+using Domain.Entities;
+using FluentValidation;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddMediatR(cfg =>
+     cfg.RegisterServicesFromAssembly(typeof(Ping).Assembly));
+
+builder.Services.AddValidatorsFromAssembly(typeof(CreateProductCommandValidator).Assembly);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +39,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseAuthentication();  
+app.UseAuthorization();   
 
 app.MapControllers();
+
+// dynamically create database in case of pending migrations
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+var pendingMigrations = await context.Database.GetPendingMigrationsAsync();
+if (pendingMigrations?.Any() == true)
+    await context.Database.MigrateAsync();
 
 app.Run();
